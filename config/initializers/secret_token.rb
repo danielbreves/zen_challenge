7638b6b184a44104e9eb3057584bf45c458b8171
Daniel Breves <djbreves@gmail.com>
2012-10-14 13:57:30 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZenChallenge::Application.config.secret_token = 'b1d5570aa2238ee1e95f243a74ea5ee3bfc6317cb4d7f54edfe60753486d790bc1bb42d195a28d5ccecb7880ec081586ef8bd01a86a4ba952ce856ad9ff9e713'
