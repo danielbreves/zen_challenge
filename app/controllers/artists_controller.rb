83d637f14692c5bed852ffc644d1dd1278be7343
Daniel Breves <djbreves@gmail.com>
2012-10-16 19:32:45 +1100

    Created actions and view to create a new artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -14,4 +14,24 @@ class ArtistsController < ApplicationController
 		  format.html
 		end
 	end
+
+	def new
+		@artist = Artist.new
+
+		respond_to do |format|
+			format.html
+		end
+	end
+
+	def create
+		@artist = Artist.new(params[:artist])
+
+		respond_to do |format|
+			if @artist.save
+				format.html { redirect_to @artist, notice: 'Artist was successfully created.' }
+			else
+				format.html { render action: "new" }
+			end
+		end
+	end
 end
