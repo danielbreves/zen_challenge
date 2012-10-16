408162a65333f8793e84e9e29f6157b9c71a3927
Daniel Breves <djbreves@gmail.com>
2012-10-16 18:46:26 +1100

    Modified seeds file to populate database

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,4 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-  attr_accessible :soundcloud, :title, :youtube
+  attr_accessible :soundcloud, :title, :youtube, :artist
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artists = Artist.create([{name: 'The Chemical Brothers'},
+						 {name: 'Red Hot Chili Peppers'},
+						 {name: 'R.E.M.'},
+						 {name: 'The Beatles'}])
+
+Song.create(title: 'Star Guitar', 			artist: artists[0])
+Song.create(title: 'Come With Us', 			artist: artists[0])
+Song.create(title: 'My Elastic Eye', 		artist: artists[0])
+Song.create(title: 'Hoops', 				artist: artists[0])
+Song.create(title: 'Californication', 		artist: artists[1])
+Song.create(title: 'Losing My Religion', 	artist: artists[2])
+Song.create(title: 'Imitation of Life',	 	artist: artists[2])
+Song.create(title: 'Yellow Submarine', 		artist: artists[3])
+Song.create(title: 'Yesterday', 			artist: artists[3])
+Song.create(title: 'Help!',					artist: artists[3])
