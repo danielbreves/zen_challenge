b3edb24b0e8dbaa3083425445a6453b74dabba19
Daniel Breves <djbreves@gmail.com>
2012-10-17 20:09:45 +1100

    Moved join_songs helper method to ArtistsHelper

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,5 @@
 module ArtistsHelper
+	def join_songs(artist)
+ 		artist.songs.map { |t| t.title }.join(", ")
+  	end
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,5 +1,3 @@
 module SongsHelper
-	def join_songs(artist)
- 		artist.songs.map { |t| t.title }.join(", ")
-  	end
+
 end
