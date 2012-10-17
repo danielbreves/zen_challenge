c58564196e2c89c44ae0c14e98f5c96f1020872c
Daniel Breves <djbreves@gmail.com>
2012-10-17 19:30:27 +1100

    Modified Artist model to accept nested attributes for songs

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,10 @@
 class Artist < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :songs_attributes
 
   validates :name, :presence => true
 
   has_many :songs, :dependent => :destroy
+
+  accepts_nested_attributes_for :songs, :allow_destroy => :true,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 end
