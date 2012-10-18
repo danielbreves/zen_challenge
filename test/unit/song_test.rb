85d8226e3a1d60e07d6e73a5ae62c3e5a4adbaad
Daniel Breves <djbreves@gmail.com>
2012-10-18 19:39:48 +1100

    Added unit tests for the Song model

--- test/unit/song_test.rb
+++ test/unit/song_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class SongTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "votes should be zero" do
+		song = Song.new
+		assert_equal song.votes, 0, "Song should be created with 0 votes"
+	end
+
+	test "should not save song without title" do
+		artist = Artist.create(name: 'The Chemical Brothers')
+		song = Song.new artist: artist
+  		assert !song.save, "Saved song without a title"
+	end
+
+	test "should not save song without artist" do
+		song = Song.new title: 'Star Guitar'
+  		assert !song.save, "Saved song without a artist"
+	end
 end
