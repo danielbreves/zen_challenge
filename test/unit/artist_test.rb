8ece4c9bfc085295686d94599a5752742cd85d35
Daniel Breves <djbreves@gmail.com>
2012-10-18 19:34:50 +1100

    Added unit tests for the Artist model

--- test/unit/artist_test.rb
+++ test/unit/artist_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ArtistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should not save artist without a name" do
+		artist = Artist.new
+  		assert !artist.save, "Saved artist without a name"
+	end
+
+	test "should destroy song when destroying artist" do
+		artist = Artist.create(name: 'The Chemical Brothers')
+		song = Song.create(title: 'Star Guitar', artist: artist)
+		artist.destroy
+  		assert !Song.exists?(song), "Left an orphan song in the database"
+	end
 end
