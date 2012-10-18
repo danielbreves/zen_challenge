32a6d84501efccbb3b05d95204d9b280d43cb7aa
Daniel Breves <djbreves@gmail.com>
2012-10-18 20:07:08 +1100

    Added functional tests for the artists controller

--- test/functional/artists_controller_test.rb
+++ test/functional/artists_controller_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class ArtistsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+  	@request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials("zen", "12345")
+    @artist = artists(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:artists)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create artist" do
+    assert_difference('Artist.count') do
+      post :create, artist: { name: @artist.name }
+    end
+
+    assert_redirected_to artist_path(assigns(:artist))
+  end
+
+  test "should show artist" do
+    get :show, id: @artist
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @artist
+    assert_response :success
+  end
+
+  test "should update artist" do
+    put :update, id: @artist, artist: { name: @artist.name }
+    assert_redirected_to artist_path(assigns(:artist))
+  end
+
+  test "should destroy artist" do
+    assert_difference('Artist.count', -1) do
+      delete :destroy, id: @artist
+    end
+
+    assert_redirected_to artists_path
+  end
 end
