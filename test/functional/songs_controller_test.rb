5aaf4aa40b8f63b13ca56fc794de575fc756c5cc
Daniel Breves <djbreves@gmail.com>
2012-10-18 19:45:47 +1100

    Added functional tests for the songs controller

--- test/functional/songs_controller_test.rb
+++ test/functional/songs_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class SongsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	setup do
+		@song = songs(:one)
+	end
+
+	test "should up vote song" do
+		assert_difference('@song.reload.votes') do
+			post :up_vote, id: @song, format: :js
+		end
+
+		assert_response :success
+	end
+
+	test "should show song" do
+		get :show, id: @song
+		assert_response :success
+	end
 end
