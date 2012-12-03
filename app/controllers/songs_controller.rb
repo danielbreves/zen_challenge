52dad4b43d4abf5e705489f50ab17fe46e2ed0e4
Daniel Ribeiro <djbreves@gmail.com>
2012-12-03 14:47:10 +1100

    Changed songs controller to respond to json format

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,20 @@
 class SongsController < ApplicationController
+	def index
+		@songs = Song.find :all
+
+		respond_to do |format|
+	      format.json { render json: @songs }
+	  end
+	end
+
 	def up_vote
 		@song = Song.find(params[:id])
 		@song.votes += 1
 		@song.save
 
 		respond_to do |format|
-	      format.js { render json: @song }
-	    end
+	      format.json { render json: @song }
+	  end
 	end
 
 	def show
@@ -14,6 +22,7 @@ class SongsController < ApplicationController
 
 		respond_to do |format|
 		  format.html
+		  format.json { render json: @song }
 		end
 	end
 end
