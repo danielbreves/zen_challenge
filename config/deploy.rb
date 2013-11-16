66ef396e74eaca3c8137acf9d6ba271d61f4d46a
Daniel Breves <djbreves@gmail.com>
2013-11-16 20:32:36 +1100

    Downgrade capistrano and switch live branch to ember_conversion

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:danielbreves/#{application}.git"
-set :branch, "master"
+set :branch, "ember_conversion"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
